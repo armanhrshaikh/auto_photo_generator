3d6b1d299c076bd7e3b0b1224f89957e0ee180e2
Arman Hr Shaikh <thearmanshaikh@gmail.com>
2024-02-08 11:01:33 +0530

    Add files via upload
    Feel free to participate to make it more easy to use.

---
+++ auto_4x6_photo_gen.bat
@@ -0,0 +1,20 @@
+@echo off
+
+::## Dependencies
+::This project relies on the following dependencies:
+::**ImageMagick:** A software suite used for creating, editing, composing, or converting bitmap images. You can download it from [ImageMagick's official website](https://imagemagick.org/script/download.php).
+::set enviroment variable 
+::done!
+
+for /f "delims=" %%a in ('powershell -command "Get-Clipboard"') do set "input1=%%a"
+
+for /f "delims=" %%a in ('wmic os get localdatetime ^| find "."') do set "datetime=%%a"
+set "datetime=%datetime:~0,14%"
+echo Generating 4x6 print-ready photo.
+
+magick montage -mode concatenate -tile 4x2 -geometry 300x375+5+5 "%input1%" "%input1%" "%input1%" "%input1%" "%input1%" "%input1%" "%input1%" "%input1%" -density 300 -units pixelsperinch "photo%datetime%.png"
+
+pause
+
+
+
